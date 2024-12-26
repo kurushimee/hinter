ccbd347c1fa96ac1e5a22c591e984492cec2f1bf
kurushimee <i@kurushimee.ru>
2024-12-26 21:28:00 +0700

    Create project

---
+++ scripts/player.gd
@@ -0,0 +1,80 @@
+extends CharacterBody3D
+
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
+const JUMP_VELOCITY = 4.5
+const GROUND_FRICTION = 7.0
+const AIR_FRICTION = 3.0
+const SENSITIVITY = 0.003
+var speed: float
+
+# Head bob
+const BOB_FREQUENCY = 2.0
+const BOB_AMPLITUDE = 0.08
+var t_bob := 0.0
+
+# FOV
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+
+@onready var head: Node3D = $Head
+@onready var camera: Camera3D = $Head/Camera3D
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, -PI / 2, PI / 2)
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Handle sprint.
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * GROUND_FRICTION)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * GROUND_FRICTION)
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * AIR_FRICTION)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * AIR_FRICTION)
+
+	# Head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = bob_offset(t_bob)
+
+	# FOV
+	var velocity_clamped: float = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov := BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+
+	move_and_slide()
+
+
+func bob_offset(time: float) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQUENCY) * BOB_AMPLITUDE
+	pos.x = cos(time * BOB_FREQUENCY / 2) * BOB_AMPLITUDE
+	return pos
