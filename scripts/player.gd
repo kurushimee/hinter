4f5f061b5f6fbc561c5c92f250b0330e05f743dc
kurushimee <jermacoff@gmail.com>
2024-12-29 20:58:19 +0700

    Remove head bob

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,10 @@
 extends CharacterBody3D
 
 # Movement constants
-const SPEED = 5.0
-const GROUND_FRICTION = 7.0
-const AIR_FRICTION = 3.0
+const SPEED = 3.0
+const GROUND_CONTROL = 10.0
+const AIR_CONTROL = 3.0
 const SENSITIVITY = 0.003
-
-# Head bob constants
-const BOB_FREQUENCY = 2.0
-const BOB_AMPLITUDE = 0.08
-
-var t_bob := 0.0
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
@@ -40,21 +34,10 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
 		else:
-			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * GROUND_FRICTION)
-			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * GROUND_FRICTION)
+			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * GROUND_CONTROL)
+			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * GROUND_CONTROL)
 	else:
-		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * AIR_FRICTION)
-		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * AIR_FRICTION)
-
-	# Head bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = bob_offset(t_bob)
+		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * AIR_CONTROL)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * AIR_CONTROL)
 
 	move_and_slide()
-
-
-func bob_offset(time: float) -> Vector3:
-	var pos = Vector3.ZERO
-	pos.y = sin(time * BOB_FREQUENCY) * BOB_AMPLITUDE
-	pos.x = cos(time * BOB_FREQUENCY / 2) * BOB_AMPLITUDE
-	return pos
