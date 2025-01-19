c237334158c86ec7517b81091bd9a2e109d663af
kurushimee <jermacoff@gmail.com>
2025-01-20 01:14:05 +0700

    Add basic day/night cycle

---
+++ scripts/day_night_cycle.gd
@@ -0,0 +1,24 @@
+extends WorldEnvironment
+
+@export var sun: Node3D
+
+@export var all_day_sky_color: Gradient
+@export var sky_material: ProceduralSkyMaterial
+@export var day_length_minutes := 10
+
+var day_length_seconds: int
+var day_time := 0.0
+
+
+func _ready() -> void:
+	day_length_seconds = day_length_minutes * 60
+
+
+func _process(delta: float) -> void:
+	day_time += delta
+
+	var day_progress: float = day_time / day_length_seconds
+	sky_material.sky_top_color = all_day_sky_color.sample(day_progress)
+	
+	# Makes the sun go from horizon to horizon
+	sun.rotation_degrees.x = -180 * day_progress
