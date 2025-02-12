c2adee78f749b7cad7e24952d9818aa85eb1006f
kurushimee <jermacoff@gmail.com>
2025-02-12 12:45:04 +0700

    Add dialogue skipping

--- scripts/managers/dialogue.gd
+++ scripts/managers/dialogue.gd
@@ -2,19 +2,45 @@ extends Node
 
 @export var dialogue_label: Label
 
-@export var print_delay := 0.01
+@export var print_delay := 0.05
+
+var is_printing := false
+var is_skipping := false
 
 
 func _ready() -> void:
+	empty()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact"):
+		skip()
+
+
+func empty() -> void:
 	dialogue_label.text = ""
 
 
 func show(text: String) -> void:
+	is_printing = true
 	dialogue_label.text = "Hinter: "
 
 	var text_left := text
 	while len(text_left) > 0:
+		if is_skipping:
+			is_skipping = false
+			dialogue_label.text = "Hinter: " + text
+			break
+
 		var letter := text_left[0]
 		text_left = text_left.erase(0)
 		dialogue_label.text += letter
 		await get_tree().create_timer(print_delay).timeout
+	is_printing = false
+
+
+func skip() -> void:
+	if is_printing:
+		is_skipping = true
+	else:
+		empty()
