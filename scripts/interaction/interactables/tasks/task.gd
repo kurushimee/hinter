fa2e58fade2748f38fcd2546a40ba8aef9fb1244
kurushimee <jermacoff@gmail.com>
2025-02-19 10:59:59 +0700

    Fix tasks after rename

--- scripts/interaction/interactables/tasks/task.gd
+++ scripts/interaction/interactables/tasks/task.gd
@@ -2,15 +2,19 @@ class_name Task
 extends Interactable
 
 
+# Set up task Interactable.
 func _ready() -> void:
 	is_active = false
 	interacted.connect(_on_interacted)
 
 
+# Universal for all tasks.
 func _on_interacted() -> void:
 	await start()
 	Events.task_completed.emit()
 
 
+# Task completion logic.
+# Task gets completed only after this function is done.
 func start() -> void:
 	pass
--- scripts/managers/tasks.gd
+++ scripts/managers/tasks.gd
@@ -1,14 +1,16 @@
 extends Node
 
-@export var all_tasks: Array[task]
+@export var all_tasks: Array[Task]
 
-var active_task: task = null
-# Difficulty affects how many tasks in a row can there be.
-var difficulty := 0
+var active_task: Task = null
 
 
-# Disables done task and clears active task.
-func complete_task() -> void:
+func _ready() -> void:
+	Events.task_completed.connect(_on_events_task_completed)
+
+
+# Disables completed task and resets active task state.
+func _on_events_task_completed() -> void:
 	active_task.is_active = false
 	active_task = null
 
@@ -20,8 +22,7 @@ func task_pending() -> bool:
 
 # Activate a random task.
 func new_task() -> void:
-	if len(all_tasks) == 0:
-		return
+	if len(all_tasks) == 0: return
 
 	active_task = all_tasks.pick_random()
 	active_task.is_active = true
