5fd3a8f90c8cf9c9fde57bacbb319cdcba367469
kurushimee <jermacoff@gmail.com>
2025-02-23 12:39:33 +0700

    Remove time limitation from task
    I forgot that tasks are time-limited. So, the player should definitely fix one first, before sleeping.

--- scripts/interaction/interactables/tasks/task.gd
+++ scripts/interaction/interactables/tasks/task.gd
@@ -1,8 +1,6 @@
 class_name Task
 extends Interactable
 
-var day_over_dialogue := "I'm too tired to do that... I need to go to sleep."
-
 
 # Set up task Interactable.
 func _ready() -> void:
@@ -11,11 +9,8 @@ func _ready() -> void:
 
 # Universal for all tasks.
 func interact() -> void:
-	if TimeManager.instance.is_day_over():
-		Events.dialogue_requested.emit(day_over_dialogue)
-	else:
-		await start()
-		Events.task_completed.emit()
+	await start()
+	Events.task_completed.emit()
 
 
 # Task completion logic.
